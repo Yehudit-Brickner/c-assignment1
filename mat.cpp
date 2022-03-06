bce7b0264fd46fed1729e53afba22bb36d012fc2
Yehudit-Brickner <nech.diti@yahoo.com>
2022-03-06 14:44:11 +0200

    changed the order of the functions

--- mat.cpp
+++ mat.cpp
@@ -14,8 +14,7 @@ using namespace std;
 // 	}
 // }
 
-void matreg(int a, int b, char c, char d );
-void matlong(int a, int b, char c, char d );
+
 
 
 /*
@@ -156,61 +155,6 @@ void matlong(int a, int b, char c, char d );
 
 */
 
-
-
-
-void mat(int a, int b, char c, char d){
-    /*
-    we will incase the function with a try and catch.
-    we will check for all reasons the function shoudnt run and throw exceptions.
-    if no exception is thrown we will check if the matrix is a long matrix or regular matrix.
-    we need to know if is long so that we cam repeat the middle row as long as needed.
-    after that we will catch the exceptions and prnt out what was wrong.
-    */
-    
-    try{
-        if(a==0 || b==0){
-            throw 1;
-        }
-        else if(a<0 || b<0){
-            throw 2;
-        }
-        else if (a%2==0 || b%2==0){
-            throw 3;
-        }
-        else if (c==d){
-            throw 4;
-        } 
-
-        else{
-            cout << "rows : " << b << " columns : " << a << endl;
-            if(a+4>b){
-                matreg(a,b,c,d);
-            }
-            else{
-                matlong(a,b,c,d);
-            }
-        }
-    }
-    catch(int e){
-         if(e==1){
-            cout << "size of row or column is 0" << endl;
-        }
-        if(e==2){
-            cout << "size of row or column is negatve" << endl;
-        }
-        if(e==3){
-            cout << "size of row or column is even" << endl;
-        }
-        if(e==4){
-            cout << "both symbols are tha sane" << endl;
-        }
-    }
-   
-}
-
-
-
 void matreg(int a, int b, char c, char d){
     
     /*
@@ -280,6 +224,7 @@ void matreg(int a, int b, char c, char d){
     }  
 
 }
+
 
 
 void matlong(int a, int b, char c, char d){
@@ -356,9 +301,134 @@ void matlong(int a, int b, char c, char d){
 }
 
 
-/*
-for long and thin try following the middle of both width and hight;
-*/
+
+void mat(int a, int b, char c, char d){
+    /*
+    we will incase the function with a try and catch.
+    we will check for all reasons the function shoudnt run and throw exceptions.
+    if no exception is thrown we will check if the matrix is a long matrix or regular matrix.
+    we need to know if is long so that we cam repeat the middle row as long as needed.
+    after that we will catch the exceptions and prnt out what was wrong.
+    */
+    
+    try{
+        if(a==0 || b==0){
+            throw 1;
+        }
+        else if(a<0 || b<0){
+            throw 2;
+        }
+        else if (a%2==0 || b%2==0){
+            throw 3;
+        }
+        else if (c==d){
+            throw 4;
+        } 
+
+        else{
+            cout << "rows : " << b << " columns : " << a << endl;
+            if(a+4>b){
+                matreg(a,b,c,d);
+            }
+            else{
+                matlong(a,b,c,d);
+            }
+        }
+    }
+    catch(int e){
+         if(e==1){
+            cout << "size of row or column is 0" << endl;
+        }
+        if(e==2){
+            cout << "size of row or column is negatve" << endl;
+        }
+        if(e==3){
+            cout << "size of row or column is even" << endl;
+        }
+        if(e==4){
+            cout << "both symbols are tha sane" << endl;
+        }
+    }
+   
+}
+
+
+
+
+
+    
+    /*
+    we will create an array of booleans the length of the matrix.
+    we will create some vals.
+    rowhalf= half the rows lower value.
+    colhalf= half the columns lower value.
+    counter= colhalf
+    we will initiate the array with all values being false.
+    we will create a nested for loop.
+    the outer loop representing the rows and the inner loop the columns.
+    we will go through the array, 
+    if the value is false we will print the first char else we will print the second char.
+    we will go down a line. and then update the array.
+    if the row is smaller or equal to half the columns of bigger or equal to (the amout of columns - half the columns -1).
+        if the row is smaller the half the rows
+            then we check if the row is odd or even.
+            in both cases we will create a for loop from the row+1, to (the amount of columns - the row -1)
+                if it is even we will change the value to true.
+                if it is odd we will change the value to false.
+        else we will do a for loop from counter to (the amount of columns -counter)
+        if the value is true we willl change to false and if false we change to true.
+        than we will decrease the counter by 1.
+    else we dont need to change what os printed because the rug is a much longer than it is wide. 
+    */
+
+    bool arr [a];
+     
+    int rowhalf=b/2;
+    int colhalf=a/2;
+    int counter=colhalf;
+
+    for (int i=0; i< a; i++){
+        arr[i]=false;
+    }
+    for (int j=0;j<b; j++){ // rows
+       for (int i=0; i< a; i++){ // columns
+            if(arr[i]==false){
+                cout << c;
+            }
+            else{
+                cout << d;
+            }
+        }
+    cout << endl;
+    //  cout << "pasthalfr: "<< pasthalfr << "  j: "<< j<<  endl;
+        if (j<colhalf || j>= b-colhalf-1){ // if not in this range we dont need to change what is printed
+            if(j<rowhalf){
+                if(j%2==0){
+                    for (int k= j+1; k<a-j-1;k++)
+                        arr[k]=true;
+                }
+                else{
+                    for (int k= j+1; k<a-j-1;k++)
+                        arr[k]=false; 
+                }
+            }
+            else{
+                for (int y=counter; y< a-counter;y++){
+                    if(arr[y]==true){
+                       arr[y]=false; 
+                    }
+                    else{
+                        arr[y]=true;
+                    }
+                }
+                counter--;                  
+            }
+        }  
+    }
+
+}
+
+
 
 
 int main(){
@@ -376,8 +446,8 @@ int main(){
     mat(7,3,'/','-'); 
     cout << "\n "<< endl;
     mat(7,9,'@','*'); 
-    // cout << " \n"<< endl;
-    // mat(5,11,'@','*'); 
+    cout << " \n"<< endl;
+    mat(5,11,'@','*'); 
     // cout << " \n"<< endl;
     // mat(7,21,'@','*'); 
     // cout << " \n"<< endl;
